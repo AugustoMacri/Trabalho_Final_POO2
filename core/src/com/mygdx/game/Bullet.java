c7d493b85e3a09a60db9b6fe345a8628b6df62cc
joaopespindola <joaopedro.espindola@outlook.com>
2023-06-02 17:16:34 -0300

    adding bullets

--- core/src/com/mygdx/game/Bullet.java
+++ core/src/com/mygdx/game/Bullet.java
@@ -1,35 +1,66 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Bullet {
 
     private int SPEED = 300; //bullet speed
-    private static Texture texture;
+    private SpriteBatch batch;
+    private Character character2;
+    private static Texture textureBullet;
+    private boolean attack;
 
-    float x, y;
+    float xBullet, yBullet;
 
-    private boolean remove = false;
-
-    public Bullet(float x, float y){
-        this.x = x;
-        this.y = y;
-
-        texture = new Texture("bullet.png");
+    public Bullet(Character character, Texture texture){
+        character2 = character;
+        batch = new SpriteBatch();
+        this.xBullet = character.getPlayerPositionX()+16;
+        this.yBullet = character.getPlayerPositionY()+16;
+        textureBullet = texture;
     }
 
-    public void update(float deltaTime){
-        x += SPEED * deltaTime;
-        if(x > Gdx.graphics.getWidth()){
-            remove = true;
+    public void generateBullet(){
+        if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+
+            attack = true;
+            yBullet = character2.getPlayerPositionY() + 16;
+        }
+        this.moveBullet();
+    }
+
+    public void moveBullet(){
+        if(attack){
+            //BALA VAI ATE O LIMITE DA TELA E DEPOIS VOLTA PARA A POSICAO INICIAL
+            if(xBullet < Gdx.graphics.getWidth()){
+                xBullet += SPEED*Gdx.graphics.getDeltaTime();
+            }else{
+                attack = false;
+            }
+        }
+        this.stopBullet();
+    }
+
+    public void stopBullet(){
+        if(attack == false) {
+            xBullet = character2.getPlayerPositionX() + 16;
+            yBullet = character2.getPlayerPositionY() + 16;
         }
     }
 
-    public void render (SpriteBatch batch){
-        batch.draw(texture, x, y);
+    public void render (){
+        batch.begin();
+        batch.draw(textureBullet, xBullet, yBullet);
+        batch.end();
     }
+
+    public void dispose(){
+        textureBullet.dispose();
+    }
+
 }
 
 
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -9,18 +10,21 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+import java.util.List;
+import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-    Texture characterTexture, cubeTexture; 
+    Texture characterTexture, cubeTexture, bulletTexture;
     
     Character character;
+    Bullet bullet;
     Cube cube;
 
     SpriteBatch spriteBatch;
+    List<Bullet> bullets;
 
 
 
@@ -30,8 +34,13 @@ public class MyGdxGame extends ApplicationAdapter {
         //creating the character
         //---------------------------------------------------------------------------------
         characterTexture = new Texture("character1.Right.png");
+        bulletTexture = new Texture("bullet.png");
         character = new Character(characterTexture, 0, 0);
         spriteBatch = new SpriteBatch();
+
+        bullet = new Bullet(character, bulletTexture);
+        bullets = new ArrayList<>();
+
 
 
         //creating the cube
@@ -48,6 +57,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 	//Set de screen back at black
 
         spriteBatch.begin();
+        bullet.render();
+        bullet.generateBullet();
         character.render();
         spriteBatch.end();
         cube.render();
@@ -58,6 +69,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void dispose(){
+        bullet.dispose();
         character.dispose();
         spriteBatch.dispose();
         cube.dispose();
