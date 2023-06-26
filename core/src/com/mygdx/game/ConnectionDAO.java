2ff0b11bb287e3df9782314829b811a214a021dd
joaopespindola <joaopedro.espindola@outlook.com>
2023-06-26 19:00:54 -0300

    fixing errors

--- core/src/com/mygdx/game/ConnectionDAO.java
+++ core/src/com/mygdx/game/ConnectionDAO.java
@@ -3,16 +3,18 @@ package com.mygdx.game;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
+import org.postgresql.Driver;
+
 
 public class ConnectionDAO {
     Connection connection = null;
     public void connect() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ranking", "root", "1234");
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://silly.db.elephantsql.com:5432/iqhizwhy", "iqhizwhy", "iYd_982gm4jk3La-lK7yBowVGupF0tN7");
 
         } catch (ClassNotFoundException e) {
-            System.out.println("Driver do BD não localizado");
+            System.out.println("Driver do BD nao localizado");
         } catch (SQLException e){
             System.out.println("Erro ao acessar o banco " + e.getMessage());
         } finally {
--- core/src/com/mygdx/game/ScriptsDAO.java
+++ core/src/com/mygdx/game/ScriptsDAO.java
@@ -1,10 +1,6 @@
 package com.mygdx.game;
 
 import java.sql.Connection;
-import java.sql.*;
-import com.mysql.cj.jdbc.Driver;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
